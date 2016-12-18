52d656a7876229af46603e875eb7fd8d5f9d2522
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-12-19 00:27:34 +0200

    Add 'docs' and change markup to reposition controls

--- js/jquery.carousel.js
+++ js/jquery.carousel.js
@@ -1,12 +1,26 @@
-// Initialize .jqcarousel() method on the container of your carousel
-// Inside the contaier, wrap your carousel items in .jqcarousel-wrapper
-// Your controls should be wrapped in container with .jqcarousel-drivers class
+// ***************
+// Natalya's Ivanova 'awesome' jquery carousel plugin
+// don't try to repeat this in real life
+// ***************
 
-// Options:
+// HOW TO USE
+// Initialize .jqcarousel() method on the container of your carousel
+// Inside the container, place .carousel-container and .carousel-controls
+// Carousel items go inside .carousel-container but should be wrapped with .jqcarousel-content
+// Width of .jqcarousel-container is custom: define it in .css file. It is how wide you carousel will be.
+// Make sure .jqcarousel-container has overflow:hidden property.
+// Width of .jqcarousel-content should be really great so that all slides would fit it.
+// Controls go inside .jqcarousel-controls container; left control should be first-child and right - last-child of the container
+
+// ***************
+// CUSTOMIZE WITH OPTIONS:
+// ***************
 // speed : speed of animation in miliseconds (500 by default)
 // easing : animation flow, string ("linear" by default)
-// cyclic: should carousel go all the way round or stop after last slide (false by default)
-// shift: step for one animation move in px. depends upon images sizes and num of images you
+// cyclic : should carousel go all the way round or stop after last slide (false by default)
+// *** cyclic here means going smoothly to the first slide when at end click right control
+// *** and go to the last slide when at start and click left control
+// shift : step for one animation move in px. if you want to shift 1 img per step, shift = image box size + margins
 
 (function($) {
 
@@ -22,12 +36,12 @@
 
         // Necessary DOM elements
         var $this = this;
-        var $wrapper = $this.find(".jqcarousel-wrapper");
-        var $left = $this.find(".jqcarousel-drivers").children(":first-child");
-        var $right = $this.find(".jqcarousel-drivers").children(":last-child");
+        var $wrapper = $this.find(".jqcarousel-content");
+        var $left = $this.find(".jqcarousel-controls").children(":first-child");
+        var $right = $this.find(".jqcarousel-controls").children(":last-child");
 
         // Calculatable values
-        var $images = $(".jqcarousel-wrapper").children().length;
+        var $images = $(".jqcarousel-content").children().length;
         var $goLeft = 0;
         var $max = defaults.shift * ($images - 3);
 
