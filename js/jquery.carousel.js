e7a3d6b68add7b7e6a12e5ef5e6251a48fb79ee0
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-12-18 21:40:19 +0200

    Add simple options

--- js/jquery.carousel.js
+++ js/jquery.carousel.js
@@ -1,14 +1,32 @@
+// Initialize .jqcarousel() method on the container of your carousel
+// Inside the contaier, wrap your carousel items in .jqcarousel-wrapper
+// Your controls should be wrapped in container with .jqcarousel-drivers class
+
+// Options:
+// speed : speed of animation in miliseconds (500 by default)
+// easing : animation flow, string ("linear" by default)
+// cyclic: should carousel go all the way round or stop after last slide (false by default)
+
 (function($) {
 
-    $.fn.jqcarousel = function() {
-        // console.log("plugin is activated");
+    $.fn.jqcarousel = function(options) {
+        var defaults = {
+            speed: 500,
+            easing: "linear",
+            cyclic: false
+        };
 
+        var settings = $.extend(defaults, options);
+
+        // Necessary DOM elements
         var $this = this;
         var $wrapper = this.find(".jqcarousel-wrapper");
-        var $goLeft = 0;
-        var $shift = 234;
         var $left = $this.find(".jqcarousel-drivers").children(":first-child");
         var $right = $this.find(".jqcarousel-drivers").children(":last-child");
+
+        // Calculatable values
+        var $goLeft = 0;
+        var $shift = 234;
 
         $left.hide();
 
@@ -16,7 +34,9 @@
             $goLeft += $shift;
             $wrapper.animate({
                 "left" : $goLeft + "px"
-            }, 500);
+            }, settings.speed,
+               settings.animation
+        );
 
             if ($goLeft == 0) {
                 $left.fadeOut();
@@ -30,7 +50,10 @@
             $goLeft -= $shift;
             $wrapper.animate({
                 "left" : $goLeft + "px"
-            }, 500);
+            }, settings.speed,
+               settings.animation
+        );
+
             if ($goLeft == $shift * -4) {
                 $right.fadeOut();
             }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $(function() {
 
-    $(".jqcarousel").jqcarousel();
+    $(".jqcarousel").jqcarousel({
+        speed: 300,
+        easing: "cubic-bezier(.17,.67,.83,.67)"
+        // animation: "ease-in-out"
+    });
 
 }); // end of ready
